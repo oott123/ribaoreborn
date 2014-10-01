f41899302f1ad67015dca328332ec8216860f6c7
oott123 <ip.192.168.1.1@qq.com>
2014-10-01 22:06:51 +0800

    防盗链保护

--- img.php
+++ img.php
@@ -3,6 +3,14 @@
     if(!preg_match('#^http://p(ic)?[0-9]+\.zhimg\.com#', $url)){
         die('Access Denied');
     }
+    //防盗链保护
+    if(!isset($_SERVER['HTTP_REFERER'])){
+        die('Welcome to zhihuribao reborn');
+    }
+    if(!preg_match('#^http://'. preg_quote($_SERVER['HTTP_HOST']).
+        '#', $_SERVER['HTTP_REFERER'])){
+        die('Hotlink protected');
+    }
     include 'include/bootstrap.inc.php';
     reborn::init();
     $cache = new SScache(reborn::$base_dir.rebornConfig::cache_dir, 36000);

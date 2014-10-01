4df3cd7abbe7da2be754f115b71f2a0600cf6236
oott123 <ip.192.168.1.1@qq.com>
2014-10-01 23:21:01 +0800

    修改rss

--- views/rss.php
+++ views/rss.php
@@ -11,10 +11,10 @@
       <title><?php echo $datum['title'];?></title>
       <description>
         <![CDATA[
-          <img src="img.php?url=<?php echo $datum['image'];?>" />
+          <img src="<?php echo $datum['image'];?>" />
           <?php
               $body = $datum['body'];
-              $body = preg_replace('#<img(.*?) src="(.*?)"#', '<img$1 src="img.php?url=$2"', $body);
+              //$body = preg_replace('#<img(.*?) src="(.*?)"#', '<img$1 src="img.php?url=$2"', $body);
               echo $body;
           ?>
           ]]>

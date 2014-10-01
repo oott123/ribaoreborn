0e82899b67bbac69e5816de0562d26ac218f4107
oott123 <ip.192.168.1.1@qq.com>
2014-10-01 21:33:01 +0800

    删除order_id（傻逼了，数据库里有啊）

--- views/index.php
+++ views/index.php
@@ -13,7 +13,7 @@
 		<div class="entry" id="entry<?php echo $datum['id'];?>">
 			<p class="title">
 				<img class="title_img" src="img.php?url=<?php echo $datum['image'];?>"/>
-				<a class="title_text" href="content.php?id=<?php echo $datum['id'];?>&amp;order_id=<?php echo $datum['order_id'];?>#content"><?php echo nl2br(htmlspecialchars($datum['title']));?></a>
+				<a class="title_text" href="content.php?id=<?php echo $datum['id'];?>#content"><?php echo nl2br(htmlspecialchars($datum['title']));?></a>
 			</p>
 		</div>
 	<?php endforeach;?>

3aa5e601c8cf5d9b727a42d316279b9ca7a5fb18
oott123 <ip.192.168.1.1@qq.com>
2014-10-05 17:41:02 +0800

    修复单选

--- views/search_box.php
+++ views/search_box.php
@@ -2,13 +2,15 @@
     <div class="first row">
         <input type="text" name="keyword" placeholder="输入关键词 空格分开"
                value="<?php echo isset($keyword)?$keyword:'';?>"/>
-        <input type="submit" name="search_title" value="标题检索"/>
-        <input type="submit" name="search_body" value="全文检索" />
+        <input type="submit" name="searchByTitle" value="标题检索"/>
+        <input type="submit" name="searchByContent" value="全文检索" />
     </div>
     <div class="second row">
-        <input type="radio" name="rank" value="by_date" id="by_date"/>
+        <input type="radio" name="rank" value="0" id="by_date"
+            <?php if(!isset($rank) || !$rank) echo 'selected';?>/>
         <label for="by_date">按日期排序</label>
-        <input type="radio" name="rank" value="by_ass" id="by_ass"/>
-        <label for="by_ass">按相关度排序</label>
+        <input type="radio" name="rank" value="1" id="by_ass"
+            <?php if(isset($rank) && $rank) echo 'selected';?>/>
+        <label for="by_ass">按相关度排序（慢）</label>
     </div>
 </div>

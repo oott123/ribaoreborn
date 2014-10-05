e4ac52be4fc01d596502008d439d8cf22ee04a12
oott123 <ip.192.168.1.1@qq.com>
2014-10-05 16:03:10 +0800

    增加搜索结果

--- views/index.php
+++ views/index.php
@@ -7,7 +7,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
 <body>
-
 	<h1><?php echo reborn::genTitle($date);?></h1>
 	<?php foreach($news as $datum):?>
 		<div class="entry" id="entry<?php echo $datum['id'];?>">
@@ -26,5 +25,10 @@
 	<?php endif;?>
 		<a href="?date=<?php echo rebornApi::get_next_day($date,-1);?>">查看前一天</a>
 	</div>
+    <div class="search">
+        <input type="text" name="keyword" placeholder="站内检索" />
+        <input type="submit" name="search_title" value="标题检索"/>
+        <input type="submit" name="search_body" value="全文检索" />
+    </div>
 </body>
 </html>

e591285474bc5394191ab3eabd1bd58c7ad94ee0
oott123 <ip.192.168.1.1@qq.com>
2014-10-05 16:07:30 +0800

    头文件独立

--- views/index.php
+++ views/index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<title>知乎日报网页阅读</title>
-	<link rel="stylesheet" href="static/general.css" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-</head>
+<?php reborn::render('view_header', array('title'=>reborn::genTitle($date)));?>
 <body>
 	<h1><?php echo reborn::genTitle($date);?></h1>
 	<?php foreach($news as $datum):
---
+++ views/view_header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title><?php echo isset($title)?$title:'知乎日报网页阅读';?></title>
+    <link rel="stylesheet" href="static/general.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+</head>
